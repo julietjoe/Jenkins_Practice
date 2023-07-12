6993d11ccc20702561bd586e3bf94a583254e947
julietjoe <julietjohny5@gmail.com>
2023-07-12 10:12:50 +0530

    commit

---
+++ jenkin_practice/src/test/java/jenkin_practice/Program1_Test.java
@@ -0,0 +1,13 @@
+package jenkin_practice;
+
+import org.testng.annotations.Test;
+
+public class Program1_Test 
+{
+	@Test
+	public void Display()
+	{
+		System.out.println("Hai Hello Welcome to Jenkins practice");
+	}
+
+}
